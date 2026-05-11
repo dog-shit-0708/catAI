85390b4b465d2ad90b6dc4efbeb17604103a25c0
dog-shit-0708 <2745756567@qq.com>
2026-05-11 14:52:06 +0800

    fix: 添加 GitHub Pages SPA 路由支持

--- WEb_cat_frontend/src/main.js
+++ WEb_cat_frontend/src/main.js
@@ -3,6 +3,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router/index.js'
 
+// GitHub Pages SPA 路由重定向处理
+const redirect = sessionStorage.redirect;
+delete sessionStorage.redirect;
+if (redirect && redirect !== location.href) {
+  history.replaceState(null, null, redirect);
+}
+
 const app = createApp(App)
 
 app.use(createPinia())
