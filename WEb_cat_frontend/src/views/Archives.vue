be97f45abd11c7874e3d39cc3068a6314fc4cd1a
dog-shit-0708 <2745756567@qq.com>
2026-05-11 14:32:40 +0800

    feat: 档案页按ID正序排列，图片null时使用默认图片

--- WEb_cat_frontend/src/views/Archives.vue
+++ WEb_cat_frontend/src/views/Archives.vue
@@ -17,7 +17,7 @@
     <div v-else-if="cats.length > 0" class="archives-grid">
       <router-link v-for="cat in cats" :key="cat.id" :to="`/cat/${cat.id}`" class="cat-card">
         <div class="cat-card-image">
-          <img :src="cat.profile_image || '/assets/images/cat_07.jpg'" :alt="cat.name">
+          <img :src="cat.profile_image ? (cat.profile_image.startsWith('http') ? cat.profile_image : '../../assets/images/' + cat.profile_image) : '../../assets/images/cat_01.jpg'" :alt="cat.name">
           <div v-if="cat.adoption_status === 1" class="status-badge adopted">已领养</div>
           <div v-else-if="cat.adoption_status === 2" class="status-badge pending">领养中</div>
           <div v-else class="status-badge available">待领养</div>
@@ -84,7 +84,8 @@ async function loadCats() {
     
     const res = await catsApi.getCats(params)
     if (res.code === 200 && res.data) {
-      cats.value = res.data.data || []
+      // 按 ID 正序排列
+      cats.value = (res.data.data || []).sort((a, b) => a.id - b.id)
       pagination.total = res.data.total || 0
     }
   } catch (err) {
